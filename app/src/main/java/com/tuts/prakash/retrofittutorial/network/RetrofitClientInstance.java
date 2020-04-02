bf40c2d26516ad6ac010c07f4e12115e3a095b5e
gdoudeng <gdoudeng@qq.com>
2020-04-02 14:02:59 +0800

    chore: 修改单例模式

--- app/src/main/java/com/tuts/prakash/retrofittutorial/network/RetrofitClientInstance.java
+++ app/src/main/java/com/tuts/prakash/retrofittutorial/network/RetrofitClientInstance.java
@@ -9,15 +9,19 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClientInstance {
 
-    private static Retrofit retrofit;
+    private volatile static Retrofit retrofit;
     private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
-            retrofit = new retrofit2.Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
+            synchronized (RetrofitClientInstance.class) {
+                if (retrofit == null) {
+                    retrofit = new retrofit2.Retrofit.Builder()
+                            .baseUrl(BASE_URL)
+                            .addConverterFactory(GsonConverterFactory.create())
+                            .build();
+                }
+            }
         }
         return retrofit;
     }
